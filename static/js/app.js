9d46e1860ffae4cc5a1204ada6109f4bfa8f3d2a
Candi Shanks <candishanks@gmail.com>
2021-04-12 18:17:17 -0400

    adding in Level_1 button and progress (filter)

--- static/js/app.js
+++ static/js/app.js
@@ -1,4 +1,40 @@
 // from data.js
 var tableData = data;
 
-// YOUR CODE HERE!
+
+//
+var tbody = d3.select("tbody");
+
+function buildTable(data) {
+
+    // clears out previous data
+    tbody.html("")
+
+
+    data.forEach((ufo_table) => {
+        var row = tbody.append("tr");
+
+        // 
+        Object.entries(ufo_table).forEach(([key, value]) =>{
+            var cell = row.append("td");
+            cell.text(value);
+        })
+    })
+}
+
+buildTable(tableData)
+
+function filterTable() {
+    // holding input value
+    var dateFilter = d3.select("#datetime").property("value");
+
+    var filteredData = tableData.filter(date => date.datetime === dateFilter);
+
+    console.log(filteredData);
+
+    buildTable(filteredData);
+
+}
+
+
+d3.selectAll("#filter-btn").on("click", filterTable)
